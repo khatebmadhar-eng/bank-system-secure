1760d33ec3ca602eb864bff1f7195f4aec1238b3
khatebmadhar-eng <khatebmadhar@gmail.com>
2026-06-22 13:05:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key' 
 
-@app.route('/')
-def home():
-    return "<h1>الموقع يعمل بنجاح!</h1>"
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username == 'admin' and password == '1234':
+            return "تم تسجيل الدخول بنجاح!"
+        else:
+            return "اسم المستخدم أو كلمة المرور خطأ!"
+    
+    return '''
+        <form method="post">
+            <input type="text" name="username" placeholder="اسم المستخدم" required><br>
+            <input type="password" name="password" placeholder="كلمة المرور" required><br>
+            <input type="submit" value="تسجيل الدخول">
+        </form>
+    '''
